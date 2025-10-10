312e21a5d04ec00065b065f33fcb8660e3a956e1
Efren Dave Cahilig <davecahilig19@gmail.com>
2025-10-10 11:53:59 +0800

    add account

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,12 +23,22 @@ namespace LMS.Controllers
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
-            if(username == "admin" && password == "password")
+            if (username == "admin" && password == "password")
             {
                 Session["Role"] = "Admin";
                 return RedirectToAction("Index", "Admin");
             }
-            ViewBag.Error = "Invalid username or password";
+            else if (username == "student" && password == "password")
+            {
+                Session["Role"] = "Student";
+                return RedirectToAction("Index", "Student");
+            }
+            else if (username == "teacher" && password == "password")
+            {
+                Session["Role"] = "Teacher";
+                return RedirectToAction("Index", "Teacher");
+            }
+                ViewBag.Error = "Invalid username or password";
             return View();
         }
     }
